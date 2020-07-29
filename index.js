d749ba567232ae5cbefce4a25f7853160bbf0bf7
victortulabot <victor_tulabot@dlsu.edu.ph>
2020-07-29 23:43:41 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// import modules express and handlebars
+const express = require('express');
+const hbs = require('hbs');
+
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 9090;
+
+// define css, img, js, and views as static 
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+// define the paths contained in routes module
+app.use('/contact-us', contact-us.html);
+
+// set hbs as view engine
+app.set('view engine', 'hbs');
+
+
+app.listen(port, function(){
+    console.log('App listening at port ' + port)
+})
+
+
