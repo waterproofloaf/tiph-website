01b9995145759fcb31dba38b82f1d99e6631e4b9
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2020-08-30 14:38:15 +0800

    Published blog/project entries works na in the website

--- controller/blogController.js
+++ controller/blogController.js
@@ -2,44 +2,46 @@ const Blog = require('../models/BlogModel.js');
 const database = require('../models/db.js');
 
 const blogController = {
-    postBlog: function(req, res){
+    postBlog: function (req, res) {
         var today = new Date();
-        
+
         var blog_title = req.body.blog_title;
         var blog_author = req.body.blog_author;
         var blog_content = req.body.blog_content;
         var blog_date = today.getMonth() + "/" + today.getDate() + "/" + today.getFullYear();
         var blog_keywords = req.body.blog_keywords;
-        
+        var blog_hide = false;
+
         var newBlog = {
             blog_title: blog_title,
             blog_author: blog_author,
             blog_content: blog_content,
             blog_date: blog_date,
-            blog_keywords: blog_keywords
+            blog_keywords: blog_keywords,
+            blog_hide: blog_hide
         }
-        
-        database.insertOne(Blog, newBlog, function(f){
-            if(f){
+
+        database.insertOne(Blog, newBlog, function (f) {
+            if (f) {
                 console.log('Blog Added: ' + blog_title);
                 res.redirect('/cms-blog');
             }
         });
     },
-    
-    findBlog: function(req, res){
+
+    findBlog: function (req, res) {
         var query = req.body.blog_title;
-        database.find({blog_title: query}, function(err, obj){
-            if(err) throw err;
+        database.find({ blog_title: query }, function (err, obj) {
+            if (err) throw err;
             console.log('Viewing: ' + query);
             res.redirect('/a-blog');
         });
     },
-    
-    deleteBlog: function(req, res){
+
+    deleteBlog: function (req, res) {
         var query = req.body.blog_title;
-        database.deleteOne(query, function(err, obj){
-            if(err) throw err;
+        database.deleteOne(query, function (err, obj) {
+            if (err) throw err;
             console.log('Blog Deleted: ' + query);
             res.redirect('/cms-blog');
         });
--- controller/projectController.js
+++ controller/projectController.js
@@ -2,16 +2,17 @@ const Project = require('../models/ProjectModel.js');
 const database = require('../models/db.js');
 
 const projectController = {
-    postProject: function(req, res){
+    postProject: function (req, res) {
         var today = new Date();
-        
+
         var proj_title = req.body.proj_title;
         var proj_content = req.body.proj_content;
         var proj_date = today.getMonth() + "/" + today.getDate() + "/" + today.getFullYear();
         var proj_keywords = req.body.proj_keywords;
         var proj_preview = req.body.proj_preview;
         var proj_status = req.body.proj_status;
-        
+        var proj_hide = false;
+
         var newProject = {
             proj_title: proj_title,
             proj_content: proj_content,
@@ -19,22 +20,23 @@ const projectController = {
             proj_keywords: proj_keywords,
             proj_preview: proj_preview,
             proj_status: proj_status,
+            proj_hide: proj_hide
         }
-        
-        database.insertOne(Project, newProject, function(f){
-            if(f){
+
+        database.insertOne(Project, newProject, function (f) {
+            if (f) {
                 console.log('Project Added: ' + proj_title);
                 res.redirect('/cms-project');
             }
         });
     },
-    
-    deleteProject: function(req, res){
+
+    deleteProject: function (req, res) {
         var query = req.body.proj_title;
-        database.deleteOne(query, function(err, obj){
-            if(err) throw err;
+        database.deleteOne(query, function (err, obj) {
+            if (err) throw err;
             console.log('Project Deleted: ' + query);
-            res.redirect('/cms-project');        
+            res.redirect('/cms-project');
         });
     }
 }
