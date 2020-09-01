c139127abe1197e85a80f6d87dfe51f63fd9c1fd
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2020-09-01 18:52:34 +0800

    Minor front-end revisions

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(express.static('views'));
 hbs.registerPartials(__dirname + '/views/partials');
 
 // define css, img, js, and views as static 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // define the paths contained in routes module
 app.use('/', routes);
@@ -36,10 +36,22 @@ app.use('/', routes);
 // connects to the database
 db.connect();
 
-app.listen(port, function(){
-    console.log('App listening at port ' + port)
+app.listen(port, function () {
+  console.log('App listening at port ' + port)
 });
 
-hbs.registerHelper('equals', function(arg1, arg2, options) {
+// Helpers
+
+hbs.registerHelper('equals', function (arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+hbs.registerHelper('each_upto', function (ary, max, options) {
+  if (!ary || ary.length == 0)
+    return options.inverse(this);
+
+  var result = [];
+  for (var i = 0; i < max && i < ary.length; ++i)
+    result.push(options.fn(ary[i]));
+  return result.join('');
 });
