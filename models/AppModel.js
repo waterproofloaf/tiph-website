1721c9890b7dbebb43c73bec31dd1ca25bcb74e0
victortulabot <victor_tulabot@dlsu.edu.ph>
2020-09-01 13:52:26 +0800

    App form adds in db (except file upload)

---
+++ models/AppModel.js
@@ -0,0 +1,80 @@
+var mongoose = require('mongoose');
+
+var AppModel = new mongoose.Schema({
+    app_firstname: {
+        type: String,
+        required: true,
+    },
+    app_lastname: {
+        type: String,
+        required: true,
+    },
+    app_nickname: {
+        type: String,
+        required: true,
+    },
+    app_email: {
+        type: String,
+        required: true,
+    },
+    app_bday: {
+        type: String,
+        required: true
+    },
+    app_cResidence: {
+        type: String,
+        required: true,
+    },
+    app_schoolcompany: {
+        type: String,
+        required: true,
+    },
+    app_number: {
+        type: String,
+        required: true,
+    },
+    app_facebook: {
+        type: String,
+        required: true,
+    },
+    app_twitter: {
+        type: String,
+    },
+    app_findout: {
+        type: String,
+        required: true,
+    },
+    app_expertise: {
+        type: String,
+        required: true,
+    },
+    app_dept: {
+        type: String,
+        required: true,
+    },
+    app_position:{
+        type: String,
+        required: true,
+    },
+    app_reason: {
+        type: String,
+        required: true,
+    },
+    app_portfolio: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('App', AppModel);
+
+exports.getAll = function (next) {
+    App.find({}).sort({}).exec(function (err, result) {
+        if (err) throw err;
+        var AppObjects = [];
+
+        result.forEach(function (doc) {
+            AppObjects.push(doc.toObject());
+        });
+        next(AppObjects);
+    })
+}
