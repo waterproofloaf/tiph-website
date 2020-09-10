eacb44d1dab6c15e2023ada404adb00d2659c480
victortulabot <victor_tulabot@dlsu.edu.ph>
2020-09-10 23:15:55 +0800

    Revert "validator for others is fixed"
    This reverts commit d85fdba63408978cb46418f94d27906075617105.

--- validators/formsValidator.js
+++ validators/formsValidator.js
@@ -27,55 +27,29 @@ const formsValidator = {
         return validation
     },
 
-    appValidation: function(req) {
-        if (req.body.app_findout == "other"){
-            var validation = [
-                check('form_agree').isIn(['Agree']).withMessage('Must agree to terms!'),
-                check('app_lastname').notEmpty().withMessage('Lastname is required!'),
-                check('app_firstname').notEmpty().withMessage('Firstname is required!'),
-                check('app_nickname').notEmpty().withMessage('Nickname is required!'),
-                check('app_email').notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid Email!'),
-                check('app_bday').notEmpty().withMessage('Birthday is required!'),
-                check('app_cResidence').notEmpty().withMessage('City of Residence is required!'),
-                check('app_schoolcompany').notEmpty().withMessage('School/Company is required!'),
-                check('app_number').notEmpty().withMessage('Number is required!').isNumeric().withMessage('Invalid Number!').isLength({min: 11, max:11}).withMessage('Invalid Number!'),
-                check('app_facebook').notEmpty().withMessage('Facebook URL is required!'),
-                check('app_others').notEmpty().withMessage('Where you found out TIPH is required!'),
-                check('app_expertise').isIn(['Legal Matters','File Organization','Minute-Taking','Human Resource Management','Project Management',
-                'Data-driven Marketing and Analysis (Social Media Engagement)', 'Content Curation (Social Media Management)', 'Website Design and Development',
-                'Content Creation (Text)', 'Content Creation (Multimedia)', 'Photography and Videography', 'Accounting', 'External Relations', 'Logistics']).
-                withMessage('Areas of Expertise is required!'),
-                check('app_dept1').notEmpty().withMessage('1st Department Choice is required!'),
-                check('app_position1').notEmpty().withMessage('1st Desired Position Choice is required!'),
-                check('app_dept2').notEmpty().withMessage('2nd Department Choice is required!'),
-                check('app_position2').notEmpty().withMessage('2nd Desired Position Choice is required!'),
-                check('app_reason').notEmpty().withMessage('Reason for Volunteering is required!').isLength({max:140}).withMessage('Answer Reason for Volunteering in 140 characters or less!'),
-            ];
-        }
-        else{
-            var validation = [
-                check('form_agree').isIn(['Agree']).withMessage('Must agree to terms!'),
-                check('app_lastname').notEmpty().withMessage('Lastname is required!'),
-                check('app_firstname').notEmpty().withMessage('Firstname is required!'),
-                check('app_nickname').notEmpty().withMessage('Nickname is required!'),
-                check('app_email').notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid Email!'),
-                check('app_bday').notEmpty().withMessage('Birthday is required!'),
-                check('app_cResidence').notEmpty().withMessage('City of Residence is required!'),
-                check('app_schoolcompany').notEmpty().withMessage('School/Company is required!'),
-                check('app_number').notEmpty().withMessage('Number is required!').isNumeric().withMessage('Invalid Number!').isLength({min: 11, max:11}).withMessage('Invalid Number!'),
-                check('app_facebook').notEmpty().withMessage('Facebook URL is required!'),
-                check('app_findout').notEmpty().withMessage('Where you found out TIPH is required!'),
-                check('app_expertise').isIn(['Legal Matters','File Organization','Minute-Taking','Human Resource Management','Project Management',
-                'Data-driven Marketing and Analysis (Social Media Engagement)', 'Content Curation (Social Media Management)', 'Website Design and Development',
-                'Content Creation (Text)', 'Content Creation (Multimedia)', 'Photography and Videography', 'Accounting', 'External Relations', 'Logistics']).
-                withMessage('Areas of Expertise is required!'),
-                check('app_dept1').notEmpty().withMessage('1st Department Choice is required!'),
-                check('app_position1').notEmpty().withMessage('1st Desired Position Choice is required!'),
-                check('app_dept2').notEmpty().withMessage('2nd Department Choice is required!'),
-                check('app_position2').notEmpty().withMessage('2nd Desired Position Choice is required!'),
-                check('app_reason').notEmpty().withMessage('Reason for Volunteering is required!').isLength({max:140}).withMessage('Answer Reason for Volunteering in 140 characters or less!'),
-            ];
-        }
+    appValidation: function() {
+        var validation = [
+            check('form_agree').isIn(['Agree']).withMessage('Must agree to terms!'),
+            check('app_lastname').notEmpty().withMessage('Lastname is required!'),
+            check('app_firstname').notEmpty().withMessage('Firstname is required!'),
+            check('app_nickname').notEmpty().withMessage('Nickname is required!'),
+            check('app_email').notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid Email!'),
+            check('app_bday').notEmpty().withMessage('Birthday is required!'),
+            check('app_cResidence').notEmpty().withMessage('City of Residence is required!'),
+            check('app_schoolcompany').notEmpty().withMessage('School/Company is required!'),
+            check('app_number').notEmpty().withMessage('Number is required!').isNumeric().withMessage('Invalid Number!').isLength({min: 11, max:11}).withMessage('Invalid Number!'),
+            check('app_facebook').notEmpty().withMessage('Facebook URL is required!'),
+            check('app_findout').notEmpty().withMessage('Where you found out TIPH is required!'),
+            check('app_expertise').isIn(['Legal Matters','File Organization','Minute-Taking','Human Resource Management','Project Management',
+            'Data-driven Marketing and Analysis (Social Media Engagement)', 'Content Curation (Social Media Management)', 'Website Design and Development',
+            'Content Creation (Text)', 'Content Creation (Multimedia)', 'Photography and Videography', 'Accounting', 'External Relations', 'Logistics']).
+            withMessage('Areas of Expertise is required!'),
+            check('app_dept1').notEmpty().withMessage('1st Department Choice is required!'),
+            check('app_position1').notEmpty().withMessage('1st Desired Position Choice is required!'),
+            check('app_dept2').notEmpty().withMessage('2nd Department Choice is required!'),
+            check('app_position2').notEmpty().withMessage('2nd Desired Position Choice is required!'),
+            check('app_reason').notEmpty().withMessage('Reason for Volunteering is required!').isLength({max:140}).withMessage('Answer Reason for Volunteering in 140 characters or less!'),
+        ];
 
         return validation
     }
